6c5d595e88ebb0ad4539bdcd51ecb2cdf790bce4
tryingeverything22 <kallikciahmett83@gmail.com>
2025-09-27 13:30:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,134 @@
+# Pyhton Starting Codes Learn Easy Pyhton
+
+
+#item1 = 'Apple'
+#item2 = 'Banana'
+#item_name ='Orange'
+
+#print(item1,item2)
+
+#print('Hello ' + item_name)
+#print('Do you want? '+ item1)
+#print('Do you like? '+ item2)
+
+#integer = 2025
+#word ='text'
+#Ishappy = False
+#naughty_list = {'Luigi','Mario','Toad' }
+#print(integer)
+#print(word)
+#print(Ishappy)
+#print(naughty_list)
+
+# More integer ---------------------------------------------------------------
+
+#integer = 2025
+#name = 'jeff'
+#string_number = '22'
+
+#print(10+int(string_number))
+
+#print (name+ str(integer))
+
+# Easy Math! -------------------------------
+
+#a= 10
+#b= 5
+
+#addition = a + b
+#difference = a- b
+#multiply = a*b
+#divide = a/b
+#power = a**b
+
+
+#print (addition)
+#print (difference)
+#print (multiply)
+#print (divide)
+#print (power)
+#------------------------------------------------------Age Trhe False Young Old
+#age = 27
+#Ishappy = False
+
+#if age > 18 and not Ishappy:
+#   print('You are old!')
+#elif age == 16:
+#    print('You are getting old!')
+#else:
+#    print('You are still young')
+
+
+#-------------------------------------- Ishappy True False (True Says You are Happy but (False Says You are not happy) )
+
+#age = 16
+#Ishappy = True
+
+#if Ishappy:
+#    print('You are happy!')
+#else:
+#    print('You are not happy!')
+
+# for i range print hello
+
+#for i in range(3):
+#    print ('Hello',i+ 1)
+
+#print(range(3))
+
+# name list easy
+
+
+#name_list = ['luigi','Mario','Toad']
+
+#for name in name_list:
+#    print(name)
+
+# app list
+
+#app_list = ["Google","Youtube","Github"]
+#for app in app_list:
+#    print(app)
+
+
+# While Loop
+
+#i = 0
+#while i < 5:
+#    i=i+1
+#    print(i)
+
+
+#while True:
+#    user_input = input('Enter something >> ')
+#    if user_input == '0':
+#        print('We are done here.')
+#        break
+
+# Def
+
+#def say_hello(name):
+#    print("Hey There", name)
+
+#say_hello('john')
+#say_hello('jeff')
+#say_hello('daniel')
+
+
+# More def
+
+#def get_internet():
+#    pass
+#def run_game():
+#    pass
+
+# numbers
+
+#number = input('Please provide a number >>')
+
+#try:
+#    print(10 + int(number))
+#except:
+#    print('That is not a valid number!')
+
+
